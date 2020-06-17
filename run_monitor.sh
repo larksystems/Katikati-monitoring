e26c558bfa522a94cf2e4b1d17946666690fd689
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2020-06-17 13:56:04 +0100

    Move pipenv run command to separate .sh file

--- run_monitor.sh
+++ run_monitor.sh
@@ -28,18 +28,6 @@ source "$(dirname $0)/find_dirs.sh"
 SESSION_NAME="$1"
 shift
 
-# Get absolute path to crypto token
-CRYPTO_TOKEN="$1"
-if [ ! -f "$CRYPTO_TOKEN" ]; then
-  echo "could not find CRYPTO_TOKEN: $CRYPTO_TOKEN"
-  exit 1
-fi
-
-cd "$(dirname "$CRYPTO_TOKEN")"
-CRYPTO_TOKEN_DIR="$(pwd)"
-CRYPTO_TOKEN_FILENAME="$(basename "$CRYPTO_TOKEN")"
-CRYPTO_TOKEN_FILE="$CRYPTO_TOKEN_DIR/$CRYPTO_TOKEN_FILENAME"
-
 # Get path to logfile
 LOGFILE="$LOGS_DIR/$SESSION_NAME-$(date "+%Y-%m-%d-%H-%M-%S").log"
 
@@ -48,7 +36,7 @@ while true; do
   echo "=============================================="
   echo ""
   echo "   Starting  : $SESSION_NAME"
-  echo "   Full path : $MONITORING_DIR/system_metrics_monitor.py"
+  echo "   Full path : $MONITORING_DIR/system_metrics_monitor.sh"
   echo "   Log file  : $LOGFILE"
   echo ""
   echo "   ctrl-c        : terminate process"
@@ -58,10 +46,10 @@ while true; do
   set +e
   # The pipe hides the exit code.
   # See https://unix.stackexchange.com/questions/14270/get-exit-status-of-process-thats-piped-to-another
-  pipenv run python system_metrics_monitor.py "$CRYPTO_TOKEN_FILE" 2>&1 | tee -a "$LOGFILE"
+  "$MONITORING_DIR/system_metrics_monitor.sh" "$@" 2>&1 | tee -a "$LOGFILE"
   MONITOR_EXIT_CODE=${PIPESTATUS[0]}
   set -e
-  echo "=== system_metrics_monitor.py has terminated with exit code $MONITOR_EXIT_CODE" | tee -a "$LOGFILE"
+  echo "=== system_metrics_monitor.sh has terminated with exit code $MONITOR_EXIT_CODE" | tee -a "$LOGFILE"
   echo "sleeping..." | tee -a "$LOGFILE"
   sleep 300
 done
